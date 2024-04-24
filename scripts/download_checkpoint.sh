94691b8b89eb38598b38eedec04522f7d9fb41c9
Federico Magistri <federico.magistri@uni-bonn.de>
2024-04-24 15:54:54 +0200

    update script to download model

--- scripts/download_checkpoint.sh
+++ scripts/download_checkpoint.sh
@@ -2,7 +2,7 @@
 
 mkdir checkpoints -p && cd checkpoints
 echo Downloading checkpoint ...
-wget -O pretrained_model.ckpt -c https://www.ipb.uni-bonn.de/html/projects/shape_completion/sweetpepper_pretrained.ckpt
+wget -O pretrained_model.ckpt -c https://www.ipb.uni-bonn.de/html/projects/shape_completion/weights_sweetpepper.pt
 
 cd ../..
 
